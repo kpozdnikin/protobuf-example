c628bd8b79896a1ec1890563c3c5386a03722f42
Konstantin Pozdnikin <kpozdnikin@gmail.com>
2021-12-14 15:02:37 +0300

    protobuf utils

---
+++ public/env.js
@@ -0,0 +1,40 @@
+// Copyright 2017-2021 @polkadot/apps, UseTech authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+(function (window) {
+  function defaults (variable, defaults) {
+    if (/^\$\{(.*)\}$/.test(variable)) {
+      if (/^\$\{(.*)\}$/.test(defaults)) {
+        return undefined;
+      }
+
+      return defaults;
+    }
+
+    switch (typeof defaults) {
+      case 'boolean':
+        if (variable === true.toString()) {
+          return true;
+        } else if (variable === false.toString()) {
+          return false;
+        } else {
+          return !!variable;
+        }
+
+      case 'number':
+        return Number(variable);
+    }
+
+    return variable || defaults;
+  }
+
+  window.ENV = window.ENV || {
+    IPFS_GATEWAY: defaults('${IPFS_GATEWAY}', 'https://dev-ipfs.unique.network/ipfs'),
+  };
+
+  // eslint-disable-next-line no-template-curly-in-string
+  window.ENV.TAG = defaults('${TAG}', '');
+
+  // eslint-disable-next-line no-template-curly-in-string
+  window.ENV.PRODUCTION = defaults('${PRODUCTION}', false);
+}(this));
